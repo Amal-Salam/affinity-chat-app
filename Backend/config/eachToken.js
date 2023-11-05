671cb8748a5bc50a0d14c911128e0b72515a1898
Nana Hauwa Abdulsalam <68066734+Amal-Salam@users.noreply.github.com>
2023-11-05 15:39:38 +0100

    Delete Backend directory

--- Backend/config/eachToken.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-
- const eachToken =(id) => {
- return jwt.sign({id}, process.env.JWT_SECRET, {
-   expiresIn: '20d', // Token expiration time
- });
- };
-module.exports = eachToken;
